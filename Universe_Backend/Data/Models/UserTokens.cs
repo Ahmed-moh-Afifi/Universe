8194cbffc9310e325ac9975eb37e2b3da796a5f8
Ahmed-moh-afifi <ahmed.moh.afifi@gmail.com>
2024-06-24 23:37:19 +0300

    House Keeping.

---
+++ Universe_Backend/Data/Models/UserTokens.cs
@@ -0,0 +1,7 @@
+﻿namespace Universe_Backend.Data.Models;
+
+public class UserTokens
+{
+    public required string AccessToken { get; set; }
+    public required string RefreshToken { get; set; }
+}
