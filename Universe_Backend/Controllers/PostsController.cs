aaa7a512392e5f58cbea499ceb4631f712c83930
Ahmed-moh-afifi <ahmed.moh.afifi@gmail.com>
2024-06-24 23:37:19 +0300

    House Keeping.

--- Universe_Backend/Controllers/PostsController.cs
+++ Universe_Backend/Controllers/PostsController.cs
@@ -1,54 +1,54 @@
-// using Microsoft.AspNetCore.Mvc;
-// using UniverseBackend.Data;
-// using UniverseBackend.Repositories;
+using Microsoft.AspNetCore.Mvc;
+using Universe_Backend.Data.Models;
+using Universe_Backend.Repositories;
 
-// namespace Universe_Backend.Controllers;
+namespace Universe_Backend.Controllers;
 
-// [ApiController]
-// [Route("[controller]")]
-// public class PostsController(IPostsRepository postsRepository, ILogger<PostsController> logger) : ControllerBase
-// {
-//     [HttpGet]
-//     [Route("{userId}")]
-//     public async Task<ActionResult<IEnumerable<Post>>> GetPosts(int userId)
-//     {
-//         return Ok(await postsRepository.GetPosts(userId));
-//     }
+[ApiController]
+[Route("[controller]")]
+public class PostsController(IPostsRepository postsRepository, ILogger<PostsController> logger) : ControllerBase
+{
+    [HttpGet]
+    [Route("{userId}")]
+    public async Task<ActionResult<IEnumerable<Post>>> GetPosts(string userId)
+    {
+        return Ok(await postsRepository.GetPosts(userId));
+    }
 
-//     [HttpGet]
-//     [Route("replies/{postId}")]
-//     public async Task<ActionResult<IEnumerable<Post>>> GetReplies(int postId)
-//     {
-//         return Ok(await postsRepository.GetReplies(postId));
-//     }
+    [HttpGet]
+    [Route("replies/{postId}")]
+    public async Task<ActionResult<IEnumerable<Post>>> GetReplies(int postId)
+    {
+        return Ok(await postsRepository.GetReplies(postId));
+    }
 
-//     [HttpPost]
-//     [Route("")]
-//     public async Task<ActionResult<int>> AddPost(Post post)
-//     {
-//         return Ok(await postsRepository.AddPost(post));
-//     }
+    [HttpPost]
+    [Route("")]
+    public async Task<ActionResult<int>> AddPost(Post post)
+    {
+        return Ok(await postsRepository.AddPost(post));
+    }
 
-//     [HttpPost]
-//     [Route("replies/")]
-//     public async Task<ActionResult<int>> AddReply(Post reply)
-//     {
-//         return Ok(await postsRepository.AddReply(reply));
-//     }
+    [HttpPost]
+    [Route("replies/")]
+    public async Task<ActionResult<int>> AddReply(Post reply)
+    {
+        return Ok(await postsRepository.AddReply(reply));
+    }
 
-//     [HttpDelete]
-//     [Route("{postId}")]
-//     public async Task<ActionResult> RemovePost(int postId)
-//     {
-//         await postsRepository.RemovePost(postId);
-//         return Ok();
-//     }
+    [HttpDelete]
+    [Route("{postId}")]
+    public async Task<ActionResult> RemovePost(int postId)
+    {
+        await postsRepository.RemovePost(postId);
+        return Ok();
+    }
 
-//     [HttpDelete]
-//     [Route("replies/{replyId}")]
-//     public async Task<ActionResult> RemoveReply(int replyId)
-//     {
-//         await postsRepository.RemoveReply(replyId);
-//         return Ok();
-//     }
-// }
+    [HttpDelete]
+    [Route("replies/{replyId}")]
+    public async Task<ActionResult> RemoveReply(int replyId)
+    {
+        await postsRepository.RemoveReply(replyId);
+        return Ok();
+    }
+}
---
+++ Universe_Backend/Services/IAuthenticationService.cs
@@ -0,0 +1,12 @@
+﻿using Universe_Backend.Data.Models;
+
+namespace Universe_Backend.Services;
+
+public interface IAuthenticationService
+{
+    public Task Register(RegisterModel model);
+    public Task<UserTokens> Login(LoginModel model);
+    public Task<UserTokens> RefreshToken(string refreshToken);
+
+    // public Task RevokeToken(string refreshToken);
+}
