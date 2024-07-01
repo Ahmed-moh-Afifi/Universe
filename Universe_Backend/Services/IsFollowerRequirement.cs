96435afdd491026042318f4796c515e85f973fbf
Ahmed-moh-afifi <ahmed.moh.afifi@gmail.com>
2024-07-01 18:40:47 +0300

    Working on IsFollower Authorization Requirement.

---
+++ Universe_Backend/Services/IsFollowerRequirement.cs
@@ -0,0 +1,8 @@
+﻿using Microsoft.AspNetCore.Authorization;
+
+namespace Universe_Backend.Services;
+
+public class IsFollowerRequirement(bool isFollowerRequirementEnabled) : IAuthorizationRequirement
+{
+    public bool IsFollowerRequirementEnabled { get; set; } = isFollowerRequirementEnabled;
+}
