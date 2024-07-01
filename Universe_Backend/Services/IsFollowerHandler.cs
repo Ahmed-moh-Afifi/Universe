bc5b762025ce135c766efa7741e774307c3c7626
Ahmed-moh-afifi <ahmed.moh.afifi@gmail.com>
2024-07-01 18:40:47 +0300

    Working on IsFollower Authorization Requirement.

---
+++ Universe_Backend/Services/IsFollowerHandler.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.EntityFrameworkCore;
+using Universe_Backend.Data;
+
+namespace Universe_Backend.Services;
+
+public class IsFollowerHandler(ApplicationDbContext dbContext) : AuthorizationHandler<IsFollowerRequirement>
+{
+    protected override async Task HandleRequirementAsync(AuthorizationHandlerContext context, IsFollowerRequirement requirement)
+    {
+        var isFollower = await dbContext.Users.Where(u => u.Id == context.User.Claims.First().Value).Select(u => new { u.Id }).ContainsAsync(new { Id = (context.Resource as string)! });
+
+    }
+}
