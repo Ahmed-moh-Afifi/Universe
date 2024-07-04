11e4aad6a4c211becd6af842d8ba8327baf11e32
Ahmed-moh-afifi <ahmed.moh.afifi@gmail.com>
2024-07-05 02:47:58 +0300

    Added DTOs for Post, PostReaction, Story, StoryReaction, Tag and User.

---
+++ Universe_Backend/Data/DTOs/UserDTO.cs
@@ -0,0 +1,42 @@
+﻿using Universe_Backend.Data.Models;
+
+namespace Universe_Backend.Data.DTOs;
+
+public class UserDTO
+{
+    public required string Id { get; set; }
+    public required string FirstName { get; set; }
+    public required string LastName { get; set; }
+    public required string UserName { get; set; }
+    public required string Email { get; set; }
+    public required bool Gender { get; set; }
+    public DateTime JoinDate { get; set; } = DateTime.Now;
+    public string? PhotoUrl { get; set; } = null;
+    public bool Verified { get; set; } = false;
+    public string? Bio { get; set; }
+    public required AccountState AccountState { get; set; }
+    public required AccountPrivacy AccountPrivacy { get; set; }
+    public DateTime? LastOnline { get; set; }
+    public required OnlineStatus OnlineStatus { get; set; } = OnlineStatus.Online;
+
+    public User ToModel()
+    {
+        return new User
+        {
+            Id = Id,
+            FirstName = FirstName,
+            LastName = LastName,
+            UserName = UserName,
+            Email = Email,
+            Gender = Gender,
+            JoinDate = JoinDate,
+            PhotoUrl = PhotoUrl,
+            Verified = Verified,
+            Bio = Bio,
+            AccountState = AccountState,
+            LastOnline = LastOnline,
+            OnlineStatus = OnlineStatus,
+            AccountPrivacy = AccountPrivacy
+        };
+    }
+}
