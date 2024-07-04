47967ecb8074495b63aca495cc4089341b471317
Ahmed-moh-afifi <ahmed.moh.afifi@gmail.com>
2024-07-05 02:47:58 +0300

    Added DTOs for Post, PostReaction, Story, StoryReaction, Tag and User.

---
+++ Universe_Backend/Data/DTOs/PostReactionDTO.cs
@@ -0,0 +1,24 @@
+﻿using Universe_Backend.Data.Models;
+
+namespace Universe_Backend.Data.DTOs;
+
+public class PostReactionDTO
+{
+    public required int Id { get; set; }
+    public required string UserId { get; set; }
+    public required string ReactionType { get; set; }
+    public DateTime ReactionDate { get; set; } = DateTime.Now;
+    public int PostId { get; set; }
+
+    public PostReaction ToModel()
+    {
+        return new PostReaction
+        {
+            Id = Id,
+            UserId = UserId,
+            ReactionType = ReactionType,
+            ReactionDate = ReactionDate,
+            PostId = PostId
+        };
+    }
+}
---
+++ Universe_Backend/Data/DTOs/StoryReactionDTO.cs
@@ -0,0 +1,24 @@
+﻿using Universe_Backend.Data.Models;
+
+namespace Universe_Backend.Data.DTOs;
+
+public class StoryReactionDTO
+{
+    public required int Id { get; set; }
+    public required string UserId { get; set; }
+    public required string ReactionType { get; set; }
+    public DateTime ReactionDate { get; set; } = DateTime.Now;
+    public int StoryId { get; set; }
+
+    public StoryReaction ToModel()
+    {
+        return new StoryReaction
+        {
+            Id = Id,
+            UserId = UserId,
+            ReactionType = ReactionType,
+            ReactionDate = ReactionDate,
+            StoryId = StoryId
+        };
+    }
+}
