7b55e720e23890192b47532a6819a0831e80048a
Ahmed-moh-afifi <ahmed.moh.afifi@gmail.com>
2024-11-21 20:30:16 +0200

    Added a new select image button to the new post page.

--- universe/lib/ui/blocs/new_post_bloc.dart
+++ universe/lib/ui/blocs/new_post_bloc.dart
@@ -22,16 +22,16 @@ class NewPostState {
   final NewPostStates previousState;
   final NewPostStates state;
   final String content;
-  final List<String> images;
-  final List<String> videos;
+  final List<MultipartFile> images;
+  final List<MultipartFile> videos;
   final String? error;
 
   NewPostState({
     required this.previousState,
     required this.state,
     required this.content,
-    required this.images,
-    required this.videos,
+    this.images = const [],
+    this.videos = const [],
     this.error,
   }) {
     saveState();
@@ -39,6 +39,24 @@ class NewPostState {
 
   void saveState() {
     RouteGenerator.newPostState = this;
+  }
+
+  NewPostState copyWith({
+    NewPostStates? previousState,
+    NewPostStates? state,
+    String? content,
+    List<MultipartFile>? images,
+    List<MultipartFile>? videos,
+    String? error,
+  }) {
+    return NewPostState(
+      previousState: previousState ?? this.previousState,
+      state: state ?? this.state,
+      content: content ?? this.content,
+      images: images ?? this.images,
+      videos: videos ?? this.videos,
+      error: error ?? this.error,
+    );
   }
 }
 
@@ -57,6 +75,10 @@ class PostEvent {
     required this.widgets,
   });
 }
+
+class SelectImagesEvent {}
+
+class SelectVideosEvent {}
 
 class NewPostBloc extends Bloc<Object, NewPostState> {
   NewPostBloc(
@@ -79,12 +101,10 @@ class NewPostBloc extends Bloc<Object, NewPostState> {
               event.videos.isNotEmpty) {
             if (!isClosed) {
               emit(
-                NewPostState(
+                state.copyWith(
                   previousState: state.state,
                   state: NewPostStates.loading,
-                  content: '',
-                  images: [],
-                  videos: [],
+                  content: event.content,
                 ),
               );
             }
@@ -107,25 +127,13 @@ class NewPostBloc extends Bloc<Object, NewPostState> {
               reactedToByCaller: false,
             );
 
-            final ImagePicker picker = ImagePicker();
+            if (state.images.isNotEmpty) {
+              var imagesUrls =
+                  await filesRepository.uploadImage(post.uid!, state.images);
+              post.images = imagesUrls.isNotEmpty ? imagesUrls : post.images;
 
-            var images = await picker.pickMultiImage();
-
-            var multipartImages = images.map((e) {
-              return MultipartFile.fromFileSync(e.path);
-            }).toList();
-
-            log('Images selected: $images', name: 'NewPostBloc');
-            log('Multipart images: $multipartImages', name: 'NewPostBloc');
-
-            log('Post: ${post.toJson()}', name: 'NewPostBloc');
-
-            // Add post files.
-            var imagesUrls =
-                await filesRepository.uploadImage(post.uid!, multipartImages);
-            post.images = imagesUrls.isNotEmpty ? imagesUrls : post.images;
-
-            log('Images uploaded: ${post.images}', name: 'NewPostBloc');
+              log('Images uploaded: ${post.images}', name: 'NewPostBloc');
+            }
 
             await postsRepository.addPost(post);
 
@@ -135,8 +143,8 @@ class NewPostBloc extends Bloc<Object, NewPostState> {
                   previousState: state.state,
                   state: NewPostStates.informative,
                   content: event.content,
-                  images: event.images,
-                  videos: event.videos,
+                  images: state.images,
+                  videos: state.videos,
                 ),
               );
             }
@@ -149,8 +157,8 @@ class NewPostBloc extends Bloc<Object, NewPostState> {
                       previousState: state.state,
                       state: NewPostStates.success,
                       content: event.content,
-                      images: event.images,
-                      videos: event.videos,
+                      images: state.images,
+                      videos: state.videos,
                     ),
                   );
                 }
@@ -174,5 +182,25 @@ class NewPostBloc extends Bloc<Object, NewPostState> {
         }
       },
     );
+
+    on<SelectImagesEvent>(
+      (event, emit) async {
+        final ImagePicker picker = ImagePicker();
+
+        var images = await picker.pickMultiImage();
+
+        var multipartImages = images.map((e) {
+          return MultipartFile.fromFileSync(e.path);
+        }).toList();
+
+        log('Images selected: $images', name: 'NewPostBloc');
+        log('Multipart images: $multipartImages', name: 'NewPostBloc');
+
+        emit(state.copyWith(images: [...state.images, ...multipartImages]));
+        log('State images: ${state.images}', name: 'NewPostBloc');
+      },
+    );
+
+    // on<SelectVideosEvent>();
   }
 }
